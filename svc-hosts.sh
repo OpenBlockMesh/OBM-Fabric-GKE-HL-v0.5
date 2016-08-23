bd620b4e75562fb7322502e6ebfb1c35103f98e3
jamesbuckett <james.buckett@gmail.com>
2016-08-23 14:07:44 +1000

    Update svc-hosts.sh

--- svc-hosts.sh
+++ svc-hosts.sh
@@ -8,6 +8,10 @@
 
 gcloud compute --project "your_project" ssh --zone "your_zone" "core-vm-endpoint" "sed -i -e 's|.*svc-.*||g' /etc/hosts"
 
+export CONTEXT=$(kubectl config view | awk '/current-context/ {print $2}')
+
+kubectl config set-context $CONTEXT --namespace=hyperledger
+
 kubectl get svc | awk '{print $3, $1".hyperledger.svc.cluster.local"}'|grep svc- > /tmp/svc-hosts
 
 gcloud compute --project "your_project" copy-files --zone "your_zone" /tmp/svc-hosts core-vm-endpoint:.
